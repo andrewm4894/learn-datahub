8bea90f6f4cfc8e08c306ec581904a9f5b763908
Andrew Maguire <andrewm4894@gmail.com>
2021-11-24 17:50:52 +0000

    dev

--- helpers/datahub_entity_emitter.py
+++ helpers/datahub_entity_emitter.py
@@ -1,4 +1,4 @@
-#%% 
+# %%
 
 import requests
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
@@ -6,16 +6,18 @@ import datahub.metadata.schema_classes as models
 import datahub.emitter.mce_builder as builder
 from datahub.emitter.rest_emitter import DatahubRestEmitter
 
-#%%
+
+# %%
 
 
 class DatahubEntityEmitter(DatahubRestEmitter):
-    
-    def __init__(self, env='DEV', datahub_actor='urn:li:corpuser:admin', dataset_platform='bigquery', dashboard_platform='bigquery',*args, **kwargs):
+
+    def __init__(self, env='DEV', datahub_actor='urn:li:corpuser:admin', dataset_platform='bigquery',
+                 dashboard_platform='bigquery', *args, **kwargs):
         super(DatahubEntityEmitter, self).__init__(*args, **kwargs)
         self.env = env
-        self.datahub_gms_server = kwargs.get('gms_server',None)
-        self.datahub_token = kwargs.get('token',None)
+        self.datahub_gms_server = kwargs.get('gms_server', None)
+        self.datahub_token = kwargs.get('token', None)
         self.datahub_actor = datahub_actor
         self.dataset_platform = dataset_platform
         self.dashboard_platform = dashboard_platform
@@ -29,8 +31,8 @@ class DatahubEntityEmitter(DatahubRestEmitter):
             return models.ChangeTypeClass.CREATE
         elif change_type == 'delete':
             return models.ChangeTypeClass.DELETE
-        else: 
-            return models.ChangeTypeClass.UPSERT 
+        else:
+            return models.ChangeTypeClass.UPSERT
 
     def update_glossary_term(self, name, definition='', source='INTERNAL', change_type='upsert'):
         mcpw = MetadataChangeProposalWrapper(
@@ -39,9 +41,9 @@ class DatahubEntityEmitter(DatahubRestEmitter):
             entityUrn=f'urn:li:glossaryTerm:{name}',
             aspectName="glossaryTermInfo",
             aspect=models.GlossaryTermInfoClass(
-                definition=definition, 
+                definition=definition,
                 termSource=source
-                )
+            )
         )
         self.emit_mcp(mcp=mcpw)
 
@@ -53,13 +55,12 @@ class DatahubEntityEmitter(DatahubRestEmitter):
             entityUrn=builder.make_user_urn(username=name),
             aspectName="corpUserInfo",
             aspect=models.CorpUserInfoClass(
-                displayName=display_name if display_name else name, 
+                displayName=display_name if display_name else name,
                 email=email,
                 active=active
-                )
+            )
         )
         self.emit_mcp(mcp=mcpw)
-
 
     def update_tag(self, name, description='', change_type='upsert'):
 
@@ -69,47 +70,46 @@ class DatahubEntityEmitter(DatahubRestEmitter):
             entityUrn=builder.make_tag_urn(name),
             aspectName="tagProperties",
             aspect=models.TagPropertiesClass(
-                name=name, 
+                name=name,
                 description=description
-                )
+            )
         )
         self.emit_mcp(mcp=mcpw)
 
-
     def update_dataset(
-        self, name, platform='bigquery', env='DEV', description=None, url=None, tags=None, change_type='upsert', owners=None, custom_properties=None, 
-        glossary_terms=None, upstream_datasets=None, links=None
-        ):
+            self, name, platform='bigquery', env='DEV', description=None, url=None, tags=None, change_type='upsert',
+            owners=None, custom_properties=None,
+            glossary_terms=None, upstream_datasets=None, links=None
+    ):
 
         dataset_urn = builder.make_dataset_urn(platform=platform, name=name, env=env)
-        
+
         mcpw = MetadataChangeProposalWrapper(
             "dataset",
             self.get_change_type(change_type),
             entityUrn=dataset_urn,
             aspectName="datasetProperties",
             aspect=models.DatasetPropertiesClass(
-                description=description, 
+                description=description,
                 externalUrl=url
-                )
+            )
         )
         self.emit_mcp(mcp=mcpw)
 
         # add tags
         if tags:
-            
             mcpw = MetadataChangeProposalWrapper(
                 "dataset",
                 self.get_change_type(change_type),
                 entityUrn=dataset_urn,
                 aspectName="globalTags",
-                aspect=models.GlobalTagsClass(tags=[models.TagAssociationClass(builder.make_tag_urn(tag)) for tag in tags])
+                aspect=models.GlobalTagsClass(
+                    tags=[models.TagAssociationClass(builder.make_tag_urn(tag)) for tag in tags])
             )
             self.emit_mcp(mcp=mcpw)
 
         # add owners
         if owners:
-
             mcpw = MetadataChangeProposalWrapper(
                 "dataset",
                 self.get_change_type(change_type),
@@ -117,16 +117,15 @@ class DatahubEntityEmitter(DatahubRestEmitter):
                 aspectName="ownership",
                 aspect=models.OwnershipClass(
                     owners=[
-                        models.OwnerClass(builder.make_user_urn(owner), type='DATAOWNER') 
+                        models.OwnerClass(builder.make_user_urn(owner), type='DATAOWNER')
                         for owner in owners
-                        ]
-                    )
+                    ]
+                )
             )
             self.emit_mcp(mcp=mcpw)
 
         # add custom properties
         if custom_properties:
-
             mcpw = MetadataChangeProposalWrapper(
                 "dataset",
                 self.get_change_type(change_type),
@@ -140,7 +139,6 @@ class DatahubEntityEmitter(DatahubRestEmitter):
 
         # add glossary terms
         if glossary_terms:
-
             mcpw = MetadataChangeProposalWrapper(
                 "dataset",
                 self.get_change_type(change_type),
@@ -148,17 +146,16 @@ class DatahubEntityEmitter(DatahubRestEmitter):
                 aspectName="glossaryTerms",
                 aspect=models.GlossaryTermsClass(
                     terms=[
-                        models.GlossaryTermAssociationClass(f'urn:li:glossaryTerm:{term}') 
+                        models.GlossaryTermAssociationClass(f'urn:li:glossaryTerm:{term}')
                         for term in glossary_terms
-                        ], 
+                    ],
                     auditStamp=models.AuditStampClass(time=0, actor=self.datahub_actor)
-                    )
+                )
             )
             self.emit_mcp(mcp=mcpw)
 
         # add upstream datasets
         if upstream_datasets:
-
             mcpw = MetadataChangeProposalWrapper(
                 "dataset",
                 self.get_change_type(change_type),
@@ -168,12 +165,12 @@ class DatahubEntityEmitter(DatahubRestEmitter):
                     upstreams=[
                         models.UpstreamClass(
                             dataset=builder.make_dataset_urn(
-                                platform=platform, 
-                                name=dataset, 
+                                platform=platform,
+                                name=dataset,
                                 env=env
-                                ), 
+                            ),
                             type=models.DatasetLineageTypeClass.TRANSFORMED
-                            ) 
+                        )
                         for dataset in upstream_datasets
                     ]
                 )
@@ -182,7 +179,6 @@ class DatahubEntityEmitter(DatahubRestEmitter):
 
         # add links
         if links:
-
             mcpw = MetadataChangeProposalWrapper(
                 "dataset",
                 self.get_change_type(change_type),
@@ -191,9 +187,9 @@ class DatahubEntityEmitter(DatahubRestEmitter):
                 aspect=models.InstitutionalMemoryClass(
                     elements=[
                         models.InstitutionalMemoryMetadataClass(
-                            url=links[link], 
+                            url=links[link],
                             description=link,
-                            createStamp=models.AuditStampClass(time=0,actor=self.datahub_actor)
+                            createStamp=models.AuditStampClass(time=0, actor=self.datahub_actor)
                         )
                         for link in links
                     ]
@@ -202,9 +198,10 @@ class DatahubEntityEmitter(DatahubRestEmitter):
             self.emit_mcp(mcp=mcpw)
 
     def update_dashboard(
-        self, name, inputs=None, title=None, description=None, change_type='upsert', platform='datastudio', tags=None, owners=None, glossary_terms=None, 
-        links=None
-        ):
+            self, name, inputs=None, title=None, description=None, change_type='upsert', platform='datastudio',
+            tags=None, owners=None, glossary_terms=None,
+            links=None, url=None
+    ):
 
         if inputs is None:
             inputs = []
@@ -220,7 +217,8 @@ class DatahubEntityEmitter(DatahubRestEmitter):
             aspectName="chartInfo",
             aspect=models.ChartInfoClass(
                 title=title if title else name,
-                description=description, 
+                description=description,
+                externalUrl=url,
                 lastModified=models.ChangeAuditStampsClass(
                     created=models.AuditStampClass(
                         time=0,
@@ -229,16 +227,16 @@ class DatahubEntityEmitter(DatahubRestEmitter):
                 ),
                 inputs=[
                     builder.make_dataset_urn(
-                        platform=self.dataset_platform, 
-                        name=input, 
+                        platform=self.dataset_platform,
+                        name=input,
                         env=self.env
-                        )
+                    )
                     for input in inputs
                 ]
             )
         )
         self.emit_mcp(mcp=mcpw)
-        
+
         # create dashboard
         mcpw = MetadataChangeProposalWrapper(
             "dashboard",
@@ -247,7 +245,8 @@ class DatahubEntityEmitter(DatahubRestEmitter):
             aspectName="dashboardInfo",
             aspect=models.DashboardInfoClass(
                 title=title if title else name,
-                description=description, 
+                description=description,
+                externalUrl=url,
                 charts=[chart_urn],
                 lastModified=models.ChangeAuditStampsClass(
                     created=models.AuditStampClass(
@@ -261,19 +260,18 @@ class DatahubEntityEmitter(DatahubRestEmitter):
 
         # add tags
         if tags:
-            
             mcpw = MetadataChangeProposalWrapper(
                 "dashboard",
                 self.get_change_type(change_type),
                 entityUrn=dashboard_urn,
                 aspectName="globalTags",
-                aspect=models.GlobalTagsClass(tags=[models.TagAssociationClass(builder.make_tag_urn(tag)) for tag in tags])
+                aspect=models.GlobalTagsClass(
+                    tags=[models.TagAssociationClass(builder.make_tag_urn(tag)) for tag in tags])
             )
             self.emit_mcp(mcp=mcpw)
 
         # add owners
         if owners:
-
             mcpw = MetadataChangeProposalWrapper(
                 "dashboard",
                 self.get_change_type(change_type),
@@ -281,16 +279,15 @@ class DatahubEntityEmitter(DatahubRestEmitter):
                 aspectName="ownership",
                 aspect=models.OwnershipClass(
                     owners=[
-                        models.OwnerClass(builder.make_user_urn(owner), type='DATAOWNER') 
+                        models.OwnerClass(builder.make_user_urn(owner), type='DATAOWNER')
                         for owner in owners
-                        ]
-                    )
+                    ]
+                )
             )
             self.emit_mcp(mcp=mcpw)
 
         # add glossary terms
         if glossary_terms:
-
             mcpw = MetadataChangeProposalWrapper(
                 "dashboard",
                 self.get_change_type(change_type),
@@ -298,17 +295,16 @@ class DatahubEntityEmitter(DatahubRestEmitter):
                 aspectName="glossaryTerms",
                 aspect=models.GlossaryTermsClass(
                     terms=[
-                        models.GlossaryTermAssociationClass(f'urn:li:glossaryTerm:{term}') 
+                        models.GlossaryTermAssociationClass(f'urn:li:glossaryTerm:{term}')
                         for term in glossary_terms
-                        ], 
+                    ],
                     auditStamp=models.AuditStampClass(time=0, actor=self.datahub_actor)
-                    )
+                )
             )
             self.emit_mcp(mcp=mcpw)
 
         # add links
         if links:
-
             mcpw = MetadataChangeProposalWrapper(
                 "dashboard",
                 self.get_change_type(change_type),
@@ -317,9 +313,9 @@ class DatahubEntityEmitter(DatahubRestEmitter):
                 aspect=models.InstitutionalMemoryClass(
                     elements=[
                         models.InstitutionalMemoryMetadataClass(
-                            url=links[link], 
+                            url=links[link],
                             description=link,
-                            createStamp=models.AuditStampClass(time=0,actor=self.datahub_actor)
+                            createStamp=models.AuditStampClass(time=0, actor=self.datahub_actor)
                         )
                         for link in links
                     ]
@@ -334,7 +330,7 @@ class DatahubEntityEmitter(DatahubRestEmitter):
         headers = {
             f'Authorization': f'Bearer {self.datahub_token}',
             'X-DataHub-Actor': self.datahub_actor
-            }
+        }
 
         graphql_query_add_tag = {
             "query": """mutation addTag($input: TagAssociationInput!) {\n
@@ -361,7 +357,7 @@ class DatahubEntityEmitter(DatahubRestEmitter):
         headers = {
             f'Authorization': f'Bearer {self.datahub_token}',
             'X-DataHub-Actor': self.datahub_actor
-            }
+        }
 
         graphql_query_remove_tag = {
             "query": """mutation removeTag($input: TagAssociationInput!) {\n
